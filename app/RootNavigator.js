e57fd0e9493f53832eb3168cd535f5538f8d654e
Moses Esan <mosesesan@hotmail.com>
2023-01-27 13:01:30 +0000

    Refactored RootNavigator to use the useNotification hook and the new  Splash Screen Component and set the app theme colours

--- app/RootNavigator.js
+++ app/RootNavigator.js
@@ -1,48 +1,31 @@
-import React, {useEffect, useState} from 'react';
-import {ActivityIndicator, Alert, useColorScheme} from "react-native";
+import React, {useEffect} from 'react';
+import {useColorScheme} from "react-native";
 import {SafeAreaProvider} from 'react-native-safe-area-context';
 
-import {NavigationContainer, useNavigation} from '@react-navigation/native';
+import {NavigationContainer} from '@react-navigation/native';
 import {createStackNavigator} from '@react-navigation/stack';
-import messaging from "@react-native-firebase/messaging";
 
 import {DefaultTheme, DarkTheme} from '@react-navigation/native';
 
-import {useMutation} from "@apollo/client";
 import Toast from "react-native-toast-message";
 
 import {useAuth} from "./modules/auth/AuthProvider";
 import {useLocation} from "./components/location/LocationProvider";
 
 import AuthStack from "./modules/auth/AuthRoute";
+import AuthSplash from "./modules/auth/components/AuthSplash";
 
 import AppStack from "./AppRoute";
-import {useTheme} from "./modules/ThemeProvider";
-import {REGISTER_FOR_NOTIFICATON} from "./modules/notification/NotificationService";
-
-import getDeviceToken, {NOTIFICATION_KEY} from "./util/getDeviceToken";
-import AsyncStorage from "@react-native-async-storage/async-storage";
+import useNotification from "./hooks/useNotification";
 
 export default function RootNavigator() {
 
-    //1 - DECLARE VARIABLES
-    const [isLoading, setIsLoading] = useState(true);
-
-    const {state: {isLoggedIn, currentUser}, getAuthState} = useAuth();
-    const {getCurrentLocation} = useLocation();
-    const {
-        textColor,
-        backgroundColor,
-        primaryColor,
-    } = useTheme()
-
     const scheme = useColorScheme();
+    const {getCurrentLocation} = useLocation();
+    const {state: {isLoggedIn, currentUser, isCheckingStatus}, setCheckStatus, getAuthState} = useAuth();
+    const [registerForPushNotifications, getInitialNotification, onNotificationOpenedApp] = useNotification();
 
-    let onCallback = onError = () => {
-        setIsLoading(false)
-    }
-    const [registerForNotification] = useMutation(REGISTER_FOR_NOTIFICATON, {onCompleted: onCompleted, onError: onError});
-
+    //1 - DECLARE VARIABLES
     //==================================================================================================
     //2 - MAIN CODE BEGINS HERE
     useEffect(() => {
@@ -53,104 +36,55 @@ export default function RootNavigator() {
 
                 //If logged in, get the current location
                 if (isLoggedIn) {
+                    let onCallback = () => setCheckStatus(false)
                     getCurrentLocation(onCallback, onCallback);
-                    await registerForPushNotificationsAsync()
+                    await registerForPushNotifications()
                     // getInitialNotification(onCallback)
                     // onNotificationOpenedApp(onCallback)
                 }
             } catch (e) {
                 alert(e)
             } finally {
-                setIsLoading(false)
+                setTimeout(() => {
+                    setCheckStatus(false)
+                }, 1000)
             }
         })();
 
     }, [isLoggedIn]);
-
-    //==================================================================================================
-    //3 - GRAPHQL HANDLERS
-    async function onCompleted(data) {
-        if (data.registerForNotification) {
-            // Save token in Asyncstorage
-            const jsonValue = JSON.stringify(true)
-            await AsyncStorage.setItem(NOTIFICATION_KEY, jsonValue)
-        }
-    }
-
-    function onError(error) {
-        alert("failed", error.message)
-    }
-
-    //==================================================================================================
-    //5 - ACTION HANDLERS
-    async function registerForPushNotificationsAsync() {
-        const {token, error} = await getDeviceToken()
-
-        if (token){
-            await registerForNotification({variables: {deviceToken: token}})
-        }else if (error){
-            alert(error)
-        }
-    }
-
-    function getInitialNotification(onDone) {
-        // Check whether an initial notification is available
-        messaging()
-            .getInitialNotification()
-            .then(remoteMessage => {
-                if (remoteMessage) {
-                    console.log(
-                        'Notification caused app to open from quit state:',
-                        remoteMessage.notification,
-                    );
-                    alert("opened from quit state")
-                }
-                onDone();
-            });
-    }
-
-    function onNotificationOpenedApp() {
-        messaging().onNotificationOpenedApp(remoteMessage => {
-            console.log(
-                'Notification caused app to open from background state:',
-                remoteMessage.notification,
-            );
-            alert("opened from background state")
-        });
-    }
-
 
     const MyTheme = {
         ...DefaultTheme,
         dark: false,
         colors: {
             ...DefaultTheme.colors,
-            primary: primaryColor,
-            background: backgroundColor,
-            card: 'rgb(255, 255, 255)',
-            text: textColor,
-            // border: 'rgb(199, 199, 204)',
+            primary: "#121212",
+            secondary: "#191919",
+            background: "#0E0E0E",
+            white: '#ffffff',
+            card: '#5162FF',
+            text: "#1C1C1C",
+            border: '#B5B5B5',
             // notification: 'rgb(255, 69, 58)',
         },
     };
 
-    // We haven't finished checking for the token yet
-    if (isLoading) return <ActivityIndicator style={[{flex: 1, alignItems: 'center', justifyContent: 'center'}]}/>
-
     const RootStack = createStackNavigator();
     return (
-        <SafeAreaProvider>
-            <NavigationContainer theme={scheme === 'dark' ? DarkTheme : MyTheme}>
-                <RootStack.Navigator>
-                    {currentUser == null ?
-                        (<RootStack.Screen name="Auth" component={AuthStack} options={{headerShown: false}}/>)
-                        :
-                        (<RootStack.Screen name="App" component={AppStack} options={{headerShown: false}}/>)
-                    }
-                </RootStack.Navigator>
-            </NavigationContainer>
-            <Toast/>
-        </SafeAreaProvider>
+        <AuthSplash isLoaded={!isCheckingStatus}>
+            <SafeAreaProvider>
+                <NavigationContainer theme={scheme === 'dark' ? DarkTheme : MyTheme}>
+                    <RootStack.Navigator>
+                        {currentUser == null ?
+                            (<RootStack.Screen name="Auth" component={AuthStack} options={{headerShown: false}}/>)
+                            :
+                            (<RootStack.Screen name="App" component={AppStack} options={{headerShown: false}}/>)
+                        }
+                    </RootStack.Navigator>
+                </NavigationContainer>
+                <Toast/>
+            </SafeAreaProvider>
+        </AuthSplash>
     );
 }
 

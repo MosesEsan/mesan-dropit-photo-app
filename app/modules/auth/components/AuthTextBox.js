6c754ae797705f15743da72275205d39df346c58
Moses Esan <mosesesan@hotmail.com>
2023-01-27 17:00:56 +0000

    Added a new AuthForm component

--- app/modules/auth/components/AuthTextBox.js
+++ app/modules/auth/components/AuthTextBox.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import {View, StyleSheet, TextInput, Text} from 'react-native';
+import {Button} from "@rneui/themed";
 
 export default function AuthTextBox(props){
     return(
@@ -8,6 +9,46 @@ export default function AuthTextBox(props){
             <TextInput placeholderTextColor={"#5c5858"} {...props} style={[styles.text, props.textStyle]}/>
         </View>
     )
+}
+
+export function AuthForm(props){
+    return(
+        <View style={[props.containerStyle]}>
+            {
+                props.fields.map((field, idx) => {
+                    return(
+                        <AuthTextBox
+                            key={`auth_text${idx}_box`}
+                            {...field}
+                            labelStyle={props.labelStyle}
+                            containerStyle={{marginBottom: ((idx + 1) === props.fields.length) ? 0 :20}}/>
+                    )
+                })
+            }
+            {props.error && <Text style={[styles.error]}>{props.error}</Text>}
+
+
+            <Button title={props.buttonTitle}
+                    onPress={props.onSubmit}
+                    loading={props.loading}
+                    disabled={props.disabled}
+                    containerStyle={[{marginTop: 20}]}
+                    buttonStyle={[styles.button, props.buttonStyle]}
+                    disabledStyle={[styles.button, props.buttonStyle]}
+                    titleStyle={styles.buttonText}/>
+        </View>
+    )
+}
+
+AuthForm.defaultProps ={
+    fields: [],
+    error: null,
+    onSubmit: null,
+    disabled: false,
+    loading: false,
+    buttonTitle: "Submit",
+    labelStyle: {},
+    buttonStyle: {},
 }
 
 AuthTextBox.defaultProps = {
@@ -34,5 +75,28 @@ const styles = StyleSheet.create({
         backgroundColor: "rgb(241, 245, 249)",
         borderRadius: 10,
         borderColor: "gray",
-    }
+    },
+
+    error:{
+        color: "red",
+        marginVertical: 12
+    },
+
+
+    button: {
+        height: 60,
+        borderRadius: 10,
+        justifyContent: "center",
+        alignItems: 'center',
+        backgroundColor: "#C55110",
+        marginTop:10
+    },
+
+    buttonText: {
+        fontWeight: "500",
+        fontSize: 17,
+        fontFamily: 'Helvetica Neue',
+        textAlign:"center",
+        color: "#fff",
+    },
 });
